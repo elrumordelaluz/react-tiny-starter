626827d7ac9dbbd91db99bc143c8219a4481c4ee
Lionel T <elrumordelaluz@hotmail.com>
2016-06-11 17:55:33 +0200

    Comment CSS Modules, add example for postCSS

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import appStyles from './App.css';
+// import appStyles from './App.css';
+var css = require('../styles.css');
 
 class App extends Component {
   render () {
     return (
-      <div className={appStyles.app}>My App</div>
+      // <div className={appStyles.app}>My App</div>
+      <div className="app">My App</div>
     );
   }
 }
